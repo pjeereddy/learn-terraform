1f3d369b3b254c4b95d6d0b4cc58f7bc6e4cdf6a
Pranayvreddy <jpranayviharreddy888@gmail.com>
2023-08-08 17:07:24 -0400

    adding dns records

--- roboshop-terraform/roboshop.tf
+++ roboshop-terraform/roboshop.tf
@@ -7,6 +7,14 @@ resource "aws_instance" "frontend" {
   }
 }
 
+resource "aws_route53_record" "frontend" {
+  zone_id = "Z07566581YBFAK47CB013"
+  name    = "frontend-dev.jdevops74.online"
+  type    = "A"
+  ttl     = "30"
+  records = [aws_instance.frontend.private_ip]
+}
+
 resource "aws_instance" "mongodb" {
   ami           = "ami-03265a0778a880afb"
   instance_type = "t3.micro"
