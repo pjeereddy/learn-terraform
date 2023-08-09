4fecbcc0e8b7ae6b86a39af5a3f5b3d0b76b673c
Pranayvreddy <jpranayviharreddy888@gmail.com>
2023-08-09 13:03:12 -0400

    variables in terraform

--- 03-vars/sample.tf
+++ 03-vars/sample.tf
@@ -5,25 +5,25 @@
 #output "fruit" {
 #  value = var.fruit
 #}
-
-variable "fruit_name" {
-  default = [ "apple", "orange"]
-}
-
-output "fruit_name" {
-  value = element(var.fruit_name, 2)
-}
-
-#variable "fruit_name-1" {
-#   default= {
-#     apple ={
-#      stock =50
-#       price =5
-#
-#   }
-#
-#   }
-#   }
-#output "fruit_name-1_apple" {
-#  value = element(var.fruit_name-1["apple"].state,0)
+#variable "fruit_name" {
+#  default = [ "apple", "orange"]
 #}
+#
+#
+#output "fruit_name" {
+#  value = element(var.fruit_name, 2)
+#}
+
+variable "fruit_name-1" {
+   default= {
+     apple ={
+      stock =50
+       price =5
+
+   }
+
+   }
+   }
+output "fruit_name-1_apple" {
+  value = try(var.fruit_name-1["apple"].state,0)
+}
