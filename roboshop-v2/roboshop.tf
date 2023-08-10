e6ae7fb042c4881c01468aa1505d08a0208814ba
Pranayvreddy <jpranayviharreddy888@gmail.com>
2023-08-10 17:03:51 -0400

    loops in terraform

--- roboshop-v2/roboshop.tf
+++ roboshop-v2/roboshop.tf
@@ -32,7 +32,7 @@ resource "aws_instance" "instance" {
   for_each = var.components
   ami = var.ami
   instance_type = "var.instance_type"
-  vpc_security_group_ids= var.vpc_security_group_ids
+  vpc_security_group_ids= "var.vpc_security_group_ids"
   tags = {
     Name = lookup(each.value, "name", null)
   }
