492afc8223281b48b8eb419a7b0a4246429e27d7
Pranayvreddy <jpranayviharreddy888@gmail.com>
2023-08-10 12:14:39 -0400

    loops in terraform

--- 04-loops/sample.tf
+++ 04-loops/sample.tf
@@ -1,8 +1,9 @@
 variable "components" {
-  catalogue = {name = "catalogue_sg" }
-  mongodb = {name = "mongodb.sg"}
+  default = {
+    catalogue = { name = "catalogue_sg" }
+    mongodb   = { name = "mongodb.sg" }
+  }
 }
-
 resource "aws_security_group" "allow_tls" {
 
   for_each = var.components
