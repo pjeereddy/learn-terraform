afe547837a32346819cf470248c37744c2585609
Pranayvreddy <jpranayviharreddy888@gmail.com>
2023-08-10 12:16:36 -0400

    loops in terraform

--- 04-loops/sample.tf
+++ 04-loops/sample.tf
@@ -7,5 +7,5 @@ variable "components" {
 resource "aws_security_group" "allow_tls" {
 
   for_each = var.components
-  name = lookup(var.components,each.key, null )
+  name = lookup(var.components,each.key["catalogue"], null )
 }
