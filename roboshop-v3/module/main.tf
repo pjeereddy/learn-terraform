aec676224146be2eb02e1bac7cdfe83ed8cb2918
Pranayvreddy <jpranayviharreddy888@gmail.com>
2023-08-13 09:01:02 -0400

    loops in terraform

--- roboshop-v3/module/main.tf
+++ roboshop-v3/module/main.tf
@@ -8,7 +8,7 @@ resource "aws_instance" "instance" {
   }
 }
 
-resource "aws_route53_record" "instance" {
+resource "aws_route53_record" "record" {
 
   zone_id = var.zone_id
   name    = "${var.name}-dev.jdevops74.online"
